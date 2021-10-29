ab8328d25101cf18134bb32e72a6b3e3af3d4f99
Isane DV <92152410+noob-dv@users.noreply.github.com>
2021-10-29 13:43:12 +0530

    Update ping.py

--- UltrauxSpam/ping.py
+++ UltrauxSpam/ping.py
@@ -1,4 +1,4 @@
-from UltraUX import UltraUX01, UltraUX02, UltraUX03, UltraUX04, UltraUX05, UltraUX06, UltraUX07, UltraUX08, UltraUX09, UltraUX10, UltraUX11, UltraUX12, UltraUX13, UltraUX14, UltraUX15, UltraUX16, UltraUX17, UltraUX18, UltraUX19, UltraUX20, SUDO_USERS
+from OpUstad import UltraUX01, UltraUX02, UltraUX03, UltraUX04, UltraUX05, UltraUX06, UltraUX07, UltraUX08, UltraUX09, UltraUX10, UltraUX11, UltraUX12, UltraUX13, UltraUX14, UltraUX15, UltraUX16, UltraUX17, UltraUX18, UltraUX19, UltraUX20, SUDO_USERS
 from telethon import events
 from time import time
 from datetime import datetime
